5043b4623a077f30786b61ef99216fbad9641ea6
Simon Huber <huber.simon@protonmail.com>
2025-12-31 15:06:24 +0100

    Update rollup.config.tiddlywiki.js

--- rollup.config.tiddlywiki.js
+++ rollup.config.tiddlywiki.js
@@ -1,11 +1,12 @@
 /**
- * Rollup config for TiddlyWiki5 Plugin Bundle
- * 
- * This creates a CommonJS bundle that can be required from TiddlyWiki's
- * module system via the codemirror6-plugin module-type.
+ * Rollup config for TiddlyWiki5 Plugin Bundle (no IIFE wrapper)
+ *
+ * Produces a CommonJS module for TiddlyWiki's module system
+ * (module-type: codemirror6-plugin).
  */
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 
 // Banner for TiddlyWiki module
 const banner = `/*\\
@@ -20,41 +21,51 @@ Built with Rollup - DO NOT EDIT DIRECTLY
 \\*/
 /*jslint node: true, browser: true */
 /*global $tw: false */
+
 "use strict";
+
 `;
 
+const twPaths = {
+  "@codemirror/state":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-state.js",
+  "@codemirror/view":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-view.js",
+  "@codemirror/language":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-language.js",
+  "@codemirror/commands":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-commands.js",
+  "@codemirror/autocomplete":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-autocomplete.js",
+  "@codemirror/lang-html":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-lang-html.js",
+  "@lezer/common": "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/lezer-common.js",
+  "@lezer/highlight":
+    "$:/plugins/BTC/tiddlywiki-codemirror-6/lib/lezer-highlight.js"
+};
+
+const external = Object.keys(twPaths);
+
 export default {
-  input: 'src/tiddlywiki-plugin-entry.ts',
+  input: "src/tiddlywiki-plugin-entry.ts",
+  external,
   output: {
-    file: 'dist/tiddlywiki-plugin/lang-tiddlywiki.js',
-    format: 'cjs',
-    exports: 'named',
+    file: "dist/tiddlywiki-plugin/lang-tiddlywiki.js",
+    format: "cjs", // <- CommonJS (no (function(){ ... })(); IIFE)
+    exports: "named",
     sourcemap: false,
     banner,
-    // Rewrite requires to use TiddlyWiki's module system
-    paths: {
-      '@codemirror/state': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-state.js',
-      '@codemirror/view': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-view.js',
-      '@codemirror/language': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-language.js',
-      '@codemirror/commands': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-commands.js',
-      '@codemirror/autocomplete': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/codemirror-autocomplete.js',
-      '@lezer/common': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/lezer-common.js',
-      '@lezer/highlight': '$:/plugins/BTC/tiddlywiki-codemirror-6/lib/lezer-highlight.js'
-    }
+    paths: twPaths,
+    // keep the output closer to TW's expectations
+    esModule: false
   },
-  external: [
-    '@codemirror/state',
-    '@codemirror/view', 
-    '@codemirror/language',
-    '@codemirror/commands',
-    '@codemirror/autocomplete',
-    '@lezer/common',
-    '@lezer/highlight'
-  ],
   plugins: [
-    resolve(),
+    resolve({
+      extensions: [".mjs", ".js", ".json", ".ts"]
+    }),
+    commonjs(),
     typescript({
-      tsconfig: './tsconfig.json',
+      tsconfig: "./tsconfig.json",
       declaration: false,
       declarationMap: false
     })
