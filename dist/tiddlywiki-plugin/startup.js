9335387b86e4700ab486d07f0b2b5b43bcff3d70
Simon Huber <huber.simon@protonmail.com>
2025-12-31 13:49:59 +0100

    Update startup.js

--- dist/tiddlywiki-plugin/startup.js
+++ dist/tiddlywiki-plugin/startup.js
@@ -3,7 +3,8 @@ title: $:/plugins/prtw/codemirror-tiddlywiki/startup.js
 type: application/javascript
 module-type: startup
 
-Registers TiddlyWiki language mode with CodeMirror 6
+Initialization for TiddlyWiki CodeMirror 6 language plugin.
+The actual plugin is loaded automatically via module-type: codemirror6-plugin
 
 \*/
 (function(){
@@ -12,33 +13,16 @@ Registers TiddlyWiki language mode with CodeMirror 6
 /*global $tw: false */
 "use strict";
 
-// Export name and startup
-exports.name = "codemirror-tiddlywiki-mode";
+exports.name = "codemirror-tiddlywiki-init";
 exports.platforms = ["browser"];
 exports.after = ["startup"];
 exports.synchronous = true;
 
 exports.startup = function() {
-    // Check if CM6 core is available
-    if (!$tw.utils.hasOwnProperty || typeof window === "undefined") {
-        return;
-    }
-
-    // Register our language with CM6 engine
-    var langTiddlyWiki;
-    try {
-        langTiddlyWiki = require("$:/plugins/prtw/codemirror-tiddlywiki/lib/lang-tiddlywiki.js");
-    } catch (e) {
-        console.warn("TiddlyWiki language mode: Could not load language module", e);
-        return;
-    }
-
-    // Make available globally for the engine
-    if (langTiddlyWiki && langTiddlyWiki.tiddlywiki) {
-        $tw.utils.CodeMirror6 = $tw.utils.CodeMirror6 || {};
-        $tw.utils.CodeMirror6.langTiddlyWiki = langTiddlyWiki;
-        
-        console.log("TiddlyWiki language mode registered with CodeMirror 6");
+    // Plugin is auto-discovered by engine.js via module-type: codemirror6-plugin
+    // This startup just logs that the plugin is available
+    if ($tw.browser) {
+        console.log("TiddlyWiki syntax plugin available for CodeMirror 6");
     }
 };
 
